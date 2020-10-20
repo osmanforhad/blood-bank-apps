758277fcb62282db4fc604a466b97b4638ac7d20
osman forhad <osmanforhad@pop-os.localdomain>
2020-10-20 16:25:19 +0600

    making splash screen and settingup project

---
+++ app/src/main/java/com/osmanforhad/bloodbank/Activities/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.osmanforhad.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.osmanforhad.bloodbank.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        /**
+         * for delay time
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+/**
+ * for go to next screen
+ */
+startActivity(new Intent(SplashScreen.this, MainActivity.class));
+finish();
+            }
+        }, 2500);
+
+    }//end of the onCreate method
+
+}//end of the SplashScreen class
